a489ec362876967bf53153904118a46b6bae07ab
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-16 23:53:41 +0530

    chore: enable CORS for multiple origins and headers

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import express from "express"
+import cors from "cors"
 
-const app = express()
+const app = express();
+
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "http://localhost:5173"],
+      credentials: true
+    })
+);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*") // watch it
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next()
+});
 
 app.listen(8000, function(){
     console.log("App is listening at port 8000...")
