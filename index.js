2ec4b6f38c01dd99b2383f798d460c064d7e5bfe
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-21 01:19:59 +0530

    chore: Update index.js to include ffmpeg command for video conversion to HLS format

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid"
 import path from "path"
 import fs from "fs"
 import { exec } from "child_process" // watch out
+import { stderr, stdout } from "process"
 
 const app = express();
 
@@ -47,15 +48,35 @@ app.get('/', function(req, res){
 
 app.post("/upload", upload.single("file"), function(req, res){
     const lessonId = uuidv4();
-    const vidioPath = req.file.path;
-    const outputPath = `uploads/courses/${lessonId}`;
+    const videoPath = req.file.path;
+    const outputPath = `./uploads/courses/${lessonId}`;
     const hlsPath = `${outputPath}/index.m3u8`;
-    const thumbPath = `${outputPath}/thumb.jpg`;
     console.log("hlsPath: ", hlsPath);
-    console.log("thumbPath: ", thumbPath);
 
-    console.log("file uploaded successfully");
-    res.json({message: "file uploaded successfully"});
+    if(!fs.existsSync(outputPath)){
+        fs.mkdirSync(outputPath, {recursive: true});
+    }
+
+    // ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}
+
+`;
+    // no queue because of POC, not to be used in production
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`exec error: ${error}`)
+        }
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+
+        const videoUrl = `http://localhost:8000/uploads/courses/${lessonId}/index.m3u8`;
+
+        res.json({
+            message: "Video converted to HLS format",
+            videoUrl: videoUrl,
+            lessonId: lessonId
+        })
+    })
 });
 
 app.listen(8000, function(){
