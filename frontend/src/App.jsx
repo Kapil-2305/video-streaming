7691afba5ce5a1900f471ae15552f6546134d0ce
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-05-21 01:26:15 +0530

    refactor: Optimize VideoPlayer component for better performance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import VideoPlayer from './VideoPlayer'
 import { useRef } from 'react'
